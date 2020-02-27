b4d605d1ef08c0b3d3b03b1c315a54fae615a1e6
Mike Chirico <mchirico@gmail.com>
2020-02-27 11:54:15 -0500

    release/0.0.6: Default ports -p 3000:3000 -p 8080:8080 -p 6379:6379 (#7)

--- dist/index.js
+++ dist/index.js
@@ -1062,13 +1062,13 @@ runDocker:
 \tcd sshDocker && \\
 \tdocker build --no-cache -t gcr.io/pigdevonlyx/docker-action:test -f Dockerfile . && \\
 \techo "ENV... \${milliseconds}" && \\
-\tdocker run -d -p 3000:3000 --rm -it --name docker-action gcr.io/pigdevonlyx/docker-action:test
+\tdocker run -d -p 3000:3000 -p 8080:8080 -p 6379:6379 --rm -it --name docker-action gcr.io/pigdevonlyx/docker-action:test
 
 runDockerND:
 \tcd sshDocker && \\
 \tdocker build --no-cache -t gcr.io/pigdevonlyx/docker-action:test -f Dockerfile . && \\
 \techo "ENV... \${milliseconds}" && \\
-\tdocker run  -p 3000:3000 --rm -it --name docker-action gcr.io/pigdevonlyx/docker-action:test
+\tdocker run  -p 3000:3000 -p 8080:8080 -p 6379:6379 --rm -it --name docker-action gcr.io/pigdevonlyx/docker-action:test
 
 push:
 \tdocker push gcr.io/pigdevonlyx/docker-action:test
